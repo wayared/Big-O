329b25905acbc5356cd69b2d49bfeda9ff8c09a8
wayared <jarecast@espol.edu.ec>
2023-04-30 16:28:15 -0700

    Big O notation

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,18 @@
+
+/*
+Big-O Notation
+
+n - The size of the input Complexities ordered in from smallest to largest
+Constant Time: O(1)
+Logarithmisc Time: O(log(n))
+Linear Time: O(n)
+Linearithmic Time: O(nlog(n))
+Quadric Time: O(n^2)
+Cubic Time: O(n^3)
+Exponential Time: O(b^n) , b>1
+Factorial Time: O(n!)
+
+ */
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
