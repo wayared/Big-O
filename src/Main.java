1896355b53faccd1b3380720a9d7411908365d27
wayared <jarecast@espol.edu.ec>
2023-05-03 15:26:42 -0700

    Big-O Examples

--- src/Main.java
+++ src/Main.java
@@ -12,11 +12,39 @@ Cubic Time: O(n^3)
 Exponential Time: O(b^n) , b>1
 Factorial Time: O(n!)
 
+Big-O Examples
+i:=0
+While i < n Do
+    j=0
+    While j <= 3*n Do
+        j = j + 1
+    j = 0
+    While j< 2*n Do
+        j = j + 1
+    i = i + 1
+
+f(n) = n * (3n + 2n) = 5n^2
+O(f(n)) = O(n^2)
+-------------------------------------
+
+i:=0
+While i < 3 * n Do
+    j: = 0
+    While j <= 50 Do
+        j = j + 1
+    j = 0
+    While j < n*n*n Do
+        j = j + 2
+    i = i + 1
+
+f(n) = 3n * (40 + n^3/2) = 3n/40 + 3n^4/2
+O(f(n)) = O(n^4)
+
  */
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
-        
+
     }
 }
